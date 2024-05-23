6f72444e91b684023498e14d0d8746eb0b1660ee
Kintan Patel <kintanpatel4393@gmail.com>
2024-05-23 08:18:12 +0530

    Initial Commit

---
+++ SIPProsper/SIPProsperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIPProsperApp.swift
+//  SIPProsper
+//
+//  Created by kintan on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SIPProsperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
